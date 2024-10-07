63b76c064040fb4d9417c21a645721d608136b02
Devang-sharmaa <fantombrothers191@gmail.com>
2024-10-07 16:27:58 +0530

    Switched to HashRouter for GitHub Pages compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
-import React from 'react'
-import {Route,Routes,BrowserRouter} from "react-router-dom"
-import Navbar from "./components/Navbar"
-import Home from './pages/Home'
-import Projects from './pages/Projects'
+import React from 'react';
+import { Route, Routes, HashRouter } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from './pages/Home';
+import Projects from './pages/Projects';
 import Contact from './pages/Contact';
-import About from './pages/About'
+import About from './pages/About';
 
 const App = () => {
   return (
     <main className='bg-slate-300/20 h-full'>
-    <BrowserRouter>
-    <Navbar/>
-    <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path='/about' element={<About/>} />
-      <Route path='/projects' element={<Projects/>} />
-      <Route path='/contact' element={<Contact/>} />
-    </Routes>
-    
-    </BrowserRouter>
+      <HashRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </HashRouter>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
+
+
