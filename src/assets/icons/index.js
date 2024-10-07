a9420aa4b69558ad9e74807be8c08c521691df95
Devang-sharmaa <fantombrothers191@gmail.com>
2024-10-07 16:09:17 +0530

    add: initial files

---
+++ src/assets/icons/index.js
@@ -0,0 +1,65 @@
+import css from './css.svg'
+import express from './express.svg'
+import git from './git.svg'
+import github from './github.svg'
+import html from './html.svg'
+import javascript from './javascript.svg'
+import mongodb from './mongodb.svg'
+import motion from './motion.svg'
+import mui from './mui.svg'
+import nextjs from './nextjs.svg'
+import nodejs from './nodejs.svg'
+import react from './react.svg'
+import redux from './redux.svg'
+import sass from './sass.svg'
+import tailwindcss from './tailwindcss.svg'
+import typescript from './typescript.svg'
+import linkedin from './linkedin.svg'
+import twitter from './twitter.svg'
+import threads from './threads.svg'
+import arrow from './arrow.svg'
+import contact from './contact.svg'
+import soundon from './soundon.png'
+import soundoff from './soundoff.png'
+import book from "./book.svg"
+import edu from "./edu.svg";
+import ecommerce from "./ecommerce.svg";
+import music from "./music-player.svg";
+import weather from "./weather.svg";
+import prisma from "./prisma.svg";
+import shadcn from "./shadcn.svg";
+import blog from "./blog.svg";
+
+export {
+    css,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    react,
+    redux,
+    sass,
+    tailwindcss,
+    typescript,
+    linkedin,
+    twitter,
+    threads,
+    arrow,
+    contact,
+    soundon,
+    soundoff,
+    book,
+    edu,
+    weather,
+    music,
+    ecommerce,
+    prisma,
+    shadcn,
+    blog
+}
